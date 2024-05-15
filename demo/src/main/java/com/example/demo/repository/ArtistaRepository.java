9dbdf491515fbbdfc2a7df520c0cdd2cbcc401f3
Kayky Cordeiro <90627779+kaykyc@users.noreply.github.com>
2024-05-15 08:27:03 -0300

    Changes

--- demo/src/main/java/com/example/demo/repository/ArtistaRepository.java
+++ demo/src/main/java/com/example/demo/repository/ArtistaRepository.java
@@ -5,6 +5,7 @@ import com.example.demo.model.Artista;
 import com.example.demo.model.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
